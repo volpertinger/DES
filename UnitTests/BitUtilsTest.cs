d85ccf2ba476fd6f8be688384066ad9b006649df
Nikolay <merzlovnik@mail.ru>
2023-03-29 23:53:28 +0300

    Inner, Outer join tests

--- UnitTests/BitUtilsTest.cs
+++ UnitTests/BitUtilsTest.cs
@@ -79,10 +79,31 @@ namespace UnitTests
             for (byte i = 0; i < maxBit; ++i)
             {
                 var numberBefore = 1u << i;
-                byte otherIndex = (byte) (maxBit - i);
+                byte otherIndex = (byte)(maxBit - i);
                 var numberAfter = 1u << otherIndex;
                 Assert.AreEqual(numberAfter, BitUtils.BitsSwitch(numberBefore, i, otherIndex));
             }
+        }
+
+        [TestMethod]
+        public void OuterJoin()
+        {
+            Assert.AreEqual(0b0011u, BitUtils.OuterJoin(0b000111, 2, 2, 6));
+            Assert.AreEqual(0b0u, BitUtils.OuterJoin(0b000111, 0, 0, 6));
+            Assert.AreEqual(0b000111u, BitUtils.OuterJoin(0b000111, 4, 2, 6));
+            Assert.AreEqual(0b1101u, BitUtils.OuterJoin(0b110000110101, 2, 2, 12));
+            Assert.AreEqual(0b110001u, BitUtils.OuterJoin(0b110000110101, 2, 4, 12));
+        }
+
+        [TestMethod]
+        public void InnerJoin()
+        {
+            Assert.AreEqual(0b0u, BitUtils.InnerJoin(0b010101, 3, 3, 6));
+            Assert.AreEqual(0b010101u, BitUtils.InnerJoin(0b010101, 0, 0, 6));
+            Assert.AreEqual(0b0101u, BitUtils.InnerJoin(0b010101, 2, 0, 6));
+            Assert.AreEqual(0b101u, BitUtils.InnerJoin(0b010101, 0, 3, 6));
+            Assert.AreEqual(0b1u, BitUtils.InnerJoin(0b011111010101, 6, 5, 12));
+            Assert.AreEqual(0b111101010u, BitUtils.InnerJoin(0b011111010101, 1, 2, 12));
         }
     }
 }
