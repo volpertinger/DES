0d2a6ae6629e333fc21f91f84cec4d7269274e55
Nikolay <merzlovnik@mail.ru>
2023-03-29 18:50:21 +0300

    GetBit

--- DES/BitUtils.cs
+++ DES/BitUtils.cs
@@ -1,7 +1,42 @@
 ﻿namespace DES
 {
-    internal class BitUtils
+    public static class BitUtils
     {
+        // ------------------------------------------------------------------------------------------------------------
+        // variabels
+        // ------------------------------------------------------------------------------------------------------------
+        private static readonly byte maxBitIndex = 31;
+        private static readonly byte minBitIndex = 0;
+        private static readonly uint firstBitMask = (uint)1 << maxBitIndex;
+        private static readonly uint lastBitMask = 1;
 
+        // ------------------------------------------------------------------------------------------------------------
+        // public
+        // ------------------------------------------------------------------------------------------------------------
+        public static bool GetBit(uint number, byte index)
+        {
+            if (IsValidBit(index))
+            {
+                if (index > maxBitIndex / 2)
+                {
+                    return ((number >> (maxBitIndex - index)) & lastBitMask) != 0;
+                }
+                else
+                {
+                    return ((number << index) & firstBitMask) != 0;
+                }
+            }
+            else
+                throw new ArgumentException(String.Format("invalid bit index {0}. Right value is: {1} to {2}.",
+                    index, minBitIndex, maxBitIndex));
+        }
+
+        // ------------------------------------------------------------------------------------------------------------
+        // private
+        // ------------------------------------------------------------------------------------------------------------
+        private static bool IsValidBit(byte bit)
+        {
+            return bit >= minBitIndex && bit <= maxBitIndex;
+        }
     }
 }
