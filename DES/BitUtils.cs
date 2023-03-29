1c2d0e6eee76275a6d6769fd25444cb22ef59264
Nikolay <merzlovnik@mail.ru>
2023-03-29 23:53:14 +0300

    Inner, outer bits join

--- DES/BitUtils.cs
+++ DES/BitUtils.cs
@@ -5,12 +5,12 @@
         // ------------------------------------------------------------------------------------------------------------
         // variabels
         // ------------------------------------------------------------------------------------------------------------
-        private static readonly byte maxBitAmount = 32;
-        private static readonly byte minBitAmount = 0;
-        private static readonly byte maxBitIndex = 31;
-        private static readonly byte minBitIndex = 0;
-        private static readonly uint firstBitMask = 1;
-        private static readonly uint lastBitMask = (uint)1 << maxBitIndex;
+        private const byte maxBitAmount = 32;
+        private const byte minBitAmount = 0;
+        private const byte maxBitIndex = 31;
+        private const byte minBitIndex = 0;
+        private const uint firstBitMask = 1;
+        private const uint lastBitMask = (uint)1 << maxBitIndex;
 
         // ------------------------------------------------------------------------------------------------------------
         // public
@@ -76,6 +76,36 @@
             return number;
         }
 
+        /// <summary>
+        /// join first firstAmount bits with last lastAmount bits from number with bit amount = length
+        /// </summary>
+        public static uint OuterJoin(uint number, byte firstAmount, byte lastAmount, byte length = maxBitAmount)
+        {
+            CustomLengthValidation(length, firstAmount + lastAmount);
+            uint result = 0;
+            for (byte i = 0; i < firstAmount; ++i)
+            {
+                result = SetBit(result, i, GetBit(number, i));
+            }
+            for (byte i = firstAmount, j = (byte)(length - lastAmount); i < firstAmount + lastAmount; ++i, ++j)
+            {
+                result = SetBit(result, i, GetBit(number, j));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Get bits between i and j in number with bit amount = length
+        /// </summary>
+        public static uint InnerJoin(uint number, byte firstAmount, byte lastAmount, byte length = maxBitAmount)
+        {
+            CustomLengthValidation(length, firstAmount + lastAmount);
+            uint result = 0;
+            for (byte i = firstAmount; i < length - lastAmount; ++i)
+                result = SetBit(result, (byte)(i - firstAmount), GetBit(number, i));
+            return result;
+        }
+
         // ------------------------------------------------------------------------------------------------------------
         // private
         // ------------------------------------------------------------------------------------------------------------
@@ -84,7 +114,7 @@
             return bit >= minBitIndex && bit <= maxBitIndex;
         }
 
-        private static bool IsValidBitAmount(byte amount)
+        private static bool IsValidBitAmount(int amount)
         {
             return amount >= minBitAmount && amount <= maxBitAmount;
         }
@@ -96,11 +126,19 @@
                     bit, minBitIndex, maxBitIndex));
         }
 
-        private static void BitAmountValidation(byte amount)
+        private static void BitAmountValidation(int amount)
         {
             if (!IsValidBitAmount(amount))
                 throw new ArgumentException(String.Format("invalid bit amount {0}. Right value is: {1} to {2}.",
                     amount, minBitAmount, maxBitAmount));
+        }
+
+        private static void CustomLengthValidation(int maxLength, int currentLength)
+        {
+            if (currentLength > maxLength || maxLength <= 1 || maxLength > maxBitAmount)
+                throw new ArgumentException(String.Format("invalid maxLength {0} or currentLength {1}." +
+                    "Length must be from 1 to {3}. And currentLength must be < maxLength",
+                    maxLength, currentLength, maxBitAmount));
         }
 
         private static uint GetPureBitValue(byte bit)
