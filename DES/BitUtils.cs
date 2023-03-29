05eaf127726c0252c47dc658f00eb4bd165e5a1d
Nikolay <merzlovnik@mail.ru>
2023-03-29 20:51:05 +0300

    Bit set\clear

--- DES/BitUtils.cs
+++ DES/BitUtils.cs
@@ -15,20 +15,32 @@
         // ------------------------------------------------------------------------------------------------------------
         public static bool GetBit(uint number, byte index)
         {
-            if (IsValidBit(index))
+            BitValidation(index);
+            if (index > maxBitIndex / 2)
             {
-                if (index > maxBitIndex / 2)
-                {
-                    return ((number >> (maxBitIndex - index)) & lastBitMask) != 0;
-                }
-                else
-                {
-                    return ((number << index) & firstBitMask) != 0;
-                }
+                return ((number >> (maxBitIndex - index)) & lastBitMask) != 0;
             }
             else
-                throw new ArgumentException(String.Format("invalid bit index {0}. Right value is: {1} to {2}.",
-                    index, minBitIndex, maxBitIndex));
+            {
+                return ((number << index) & firstBitMask) != 0;
+            }
+
+        }
+
+        public static uint SetBit(uint number, byte index)
+        {
+            BitValidation(index);
+            if (!GetBit(number, index))
+                number += GetPureBitValue(index);
+            return number;
+        }
+
+        public static uint ClearBit(uint number, byte index)
+        {
+            BitValidation(index);
+            if (GetBit(number, index))
+                number -= GetPureBitValue(index);
+            return number;
         }
 
         // ------------------------------------------------------------------------------------------------------------
@@ -37,6 +49,18 @@
         private static bool IsValidBit(byte bit)
         {
             return bit >= minBitIndex && bit <= maxBitIndex;
+        }
+
+        private static void BitValidation(byte bit)
+        {
+            if (!IsValidBit(bit))
+                throw new ArgumentException(String.Format("invalid bit index {0}. Right value is: {1} to {2}.",
+                    bit, minBitIndex, maxBitIndex));
+        }
+
+        private static uint GetPureBitValue(byte bit)
+        {
+            return lastBitMask << (maxBitIndex - bit);
         }
     }
 }
