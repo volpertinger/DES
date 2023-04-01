dc4299a5b515cc6000fcf1ce146b413f6f194052
Nikolay <merzlovnik@mail.ru>
2023-04-01 17:23:05 +0300

    Block tests

--- UnitTests/DESTest.cs
+++ UnitTests/DESTest.cs
@@ -8,45 +8,54 @@
         [TestMethod]
         public void BlockEncryptDecrypt()
         {
-            ulong key = 32;
-            var ss = new DES.DES.DES(key);
-            ulong block = 0;
-            ulong encrypted = 0;
-            ulong plain = 0;
-
-            // block right half check
-            for (int i = 0; i < halfBlockSize; ++i)
+            var keys = new List<ulong>();
+            var testBlocks = new List<ulong>() { ulong.MinValue, ulong.MaxValue, 10, 100, 1000000000, 127, 1023, 2047,
+                4095, 4097, 11235123, 32461281, 27272727819199020, 3789323232, 217748648, 9918838178,
+                0b01111111_01111111_01111111_01111111_01111111_01111111_01111111_01111111,
+                0b11111111_11111111_11111111_11111111_00000000_00000000_00000000_00000000,
+                0b00000000_00000000_00000000_00000000_11111111_11111111_11111111_11111111};
+            var random = new Random();
+            for (int i = 0; i < 100; ++i)
             {
-                block = 1ul << i;
-                encrypted = ss.EncryptBlock(block);
-                plain = ss.DecryptBlock(encrypted);
-                Assert.AreEqual(block, plain);
-                //Assert.AreNotEqual(plain, encrypted);
-                //Assert.AreNotEqual(block, encrypted);
+                keys.Add(((uint)random.Next(int.MaxValue) << halfBlockSize) + (uint)random.Next(int.MaxValue));
+            }
+            for (int i = 0; i < keys.Count; ++i)
+            {
+                var ss = new DES.DES.DES(keys[i]);
+                for (int j = 0; j < halfBlockSize; ++j)
+                {
+                    // block right half check
+                    var block = 1ul << j;
+                    EncryptBlockTest(ss, block);
+
+                    // block left half check
+                    block = 1ul << (j + halfBlockSize);
+                    EncryptBlockTest(ss, block);
+
+                    // both blocks check
+                    block = (1ul << (j + halfBlockSize)) + (1ul << j) + (ulong)(j / 2);
+                    EncryptBlockTest(ss, block);
+                }
+                // other tests
+                for (int j = 0; j < testBlocks.Count; ++j)
+                {
+                    var block = testBlocks[j];
+                    EncryptBlockTest(ss, block);
+                }
             }
 
+        }
 
-            // block left half check
-            for (int i = halfBlockSize; i < blockSize; ++i)
-            {
-                block = 1ul << i;
-                encrypted = ss.EncryptBlock(block);
-                plain = ss.DecryptBlock(encrypted);
-                Assert.AreEqual(block, plain);
-                //Assert.AreNotEqual(plain, encrypted);
-                //Assert.AreNotEqual(block, encrypted);
-            }
-
-            // both blocks check
-            for (int i = 0; i < halfBlockSize; ++i)
-            {
-                block = (1ul << (i + halfBlockSize)) + (1ul << i) + (ulong)(i / 2);
-                encrypted = ss.EncryptBlock(block);
-                plain = ss.DecryptBlock(encrypted);
-                Assert.AreEqual(block, plain);
-                //Assert.AreNotEqual(plain, encrypted);
-                //Assert.AreNotEqual(block, encrypted);
-            }
+        // ------------------------------------------------------------------------------------------------------------
+        // Utils
+        // ------------------------------------------------------------------------------------------------------------
+        public static void EncryptBlockTest(DES.DES.DES des, ulong block)
+        {
+            var encrypted = des.EncryptBlock(block);
+            var plain = des.DecryptBlock(encrypted);
+            Assert.AreEqual(block, plain);
+            Assert.AreNotEqual(plain, encrypted);
+            Assert.AreNotEqual(block, encrypted);
         }
     }
 }
