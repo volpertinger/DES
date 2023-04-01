0ea9029ef234c446a5dacaf6e9dc88b2fb6fc76a
Nikolay <merzlovnik@mail.ru>
2023-04-01 16:00:48 +0300

    Block tests

--- UnitTests/DESTest.cs
+++ UnitTests/DESTest.cs
@@ -3,10 +3,50 @@
     [TestClass]
     public class DESTest
     {
+        private static int blockSize = 64;
+        private static int halfBlockSize = blockSize / 2;
         [TestMethod]
-        public void main()
+        public void BlockEncryptDecrypt()
         {
-            Assert.AreEqual(true, true);
+            ulong key = 32;
+            var ss = new DES.DES.DES(key);
+            ulong block = 0;
+            ulong encrypted = 0;
+            ulong plain = 0;
+
+            // block right half check
+            for (int i = 0; i < halfBlockSize; ++i)
+            {
+                block = 1ul << i;
+                encrypted = ss.EncryptBlock(block);
+                plain = ss.DecryptBlock(encrypted);
+                Assert.AreEqual(block, plain);
+                //Assert.AreNotEqual(plain, encrypted);
+                //Assert.AreNotEqual(block, encrypted);
+            }
+
+
+            // block left half check
+            for (int i = halfBlockSize; i < blockSize; ++i)
+            {
+                block = 1ul << i;
+                encrypted = ss.EncryptBlock(block);
+                plain = ss.DecryptBlock(encrypted);
+                Assert.AreEqual(block, plain);
+                //Assert.AreNotEqual(plain, encrypted);
+                //Assert.AreNotEqual(block, encrypted);
+            }
+
+            // both blocks check
+            for (int i = 0; i < halfBlockSize; ++i)
+            {
+                block = (1ul << (i + halfBlockSize)) + (1ul << i) + (ulong)(i / 2);
+                encrypted = ss.EncryptBlock(block);
+                plain = ss.DecryptBlock(encrypted);
+                Assert.AreEqual(block, plain);
+                //Assert.AreNotEqual(plain, encrypted);
+                //Assert.AreNotEqual(block, encrypted);
+            }
         }
     }
 }
